d6c365077de17136372da402e3888fca33955cdb
JunOh <JunOh@192.168.1.68>
2014-11-27 15:25:15 -0800

    Parser few function modified to be more robust

--- mainpage/parser.py
+++ mainpage/parser.py
@@ -4,38 +4,46 @@ import re
 import time
 import json
 import datetime
+from datetime import date, timedelta
 # from pip.basecommand import open_logfile
 
 class MarketParser:
     def __init__(self, url):
-        response = urllib2.urlopen(url)
-        time.sleep(3)
-        self.cr = csv.reader(response, quotechar='"', delimiter=',',
-                     quoting=csv.QUOTE_ALL, skipinitialspace=True) 
+        response = urllib2.Request(url)
+        try:
+            print "initialize using: " and url
+            result = urllib2.urlopen(response, timeout=10)
+            time.sleep(3)
+            self.cr = csv.reader(result, quotechar='"', delimiter=',',
+            quoting=csv.QUOTE_ALL, skipinitialspace=True)
+        except Exception:
+            print "Fatal Error!"
                
     # Retrieve CSV file from the Vancouver OpenData url
-    def getCSV(self): 
-        return self.cr
+    def getCSV(self):
+        try:
+            return self.cr
+        except AttributeError:
+            print "CSV file not present!"
     
     # Check if time is 1AM or not and return boolean
-    def isTimeToUpdate(self):
-        print "in update"
-        now = datetime.datetime.now()
-        
-        today1am = now.replace(hour=1, minute=0, second=0, microsecond=0)
-        print today1am
-        today1am5 = now.replace(hour=1 , minute=5, second=0, microsecond=0)
-        print today1am5
-        print now
-        print (now >= today1am or now <= today1am5)
-        return (now >= today1am or now <= today1am5)
-        
-    # First row has the last updated time of this data
-    def getUpdateRow(self, cr):
-        return cr.next()
-
-    # Remove the row with field names
-    def removeFieldNames(self, cr):
+    def updateTime(self):
+        print "time now"
+        compareDate = datetime.datetime(year=2014, month=11, day=5, hour=00).date()
+        now = date.today()
+        diff = now - timedelta(days=7)
+        print diff
+        if (diff > compareDate):
+            compareDate = now
+            print "Let's Update!"
+            return True
+        else:
+            print "Not yet!"
+            return False
+                
+    # Remove first row with update time and second row with fieldnames
+    def removeUpdateRowandFieldNames(self, cr):
+        cr.next()
         return cr.next()
 
     # If the field is empty, then return the list with empty field filled as 'N/A'
@@ -46,17 +54,6 @@ class MarketParser:
                 field = 'N/A'
             checkedOfferings.append(field)
         return checkedOfferings
-
-    # If the stored date of the updated date is different than the current date, then parse the information again
-    def checkUpdate(self, cr, storeddate, updatedate, i):
-        if(storeddate != updatedate):
-            return self.parseMarketInfo(self, cr)
-            # Keep how many updates have happened
-            i += 1
-    
-    # Get updated date from the first row of the csv file
-    def getUpdateDate(self, updaterow):
-        return updaterow[1]
     
     # Geocode address string to get google maps 
     def geocodeMarket(self, listOflat, listOflon, address):
@@ -74,83 +71,81 @@ class MarketParser:
         #append lat/lon to lists    
         listOflat.append(lat)
         listOflon.append(lon)
-        
+    
+    # Precond: cr is not None
+    # Assumption: the csv file from the vancouver db is consistent in its format
     # Parses the required market information into the corresponding lists
-    def parseMarketInfo(self, cr, lastUpdateDate, updatedate, updateCount):
-        # Check if updated date has changed (FOR NOW)
-        if (lastUpdateDate != updatedate):
-        
-        # Check if it's time to update
-        # if(self.isTimeToUpdate()):
-            # Increment update count
-            updateCount += 1
+    def parseMarketInfo(self, cr):
+        # Create lists to store market info
+        names = []
+        types = []
+        organizations = []
+        addresses = []
+        websites = []
+        open_days = []
+        open_times = []
+        close_times = []
+        open_months = []
+        vendor_numbers = []
+        offerings = []
+        lats = []
+        lons = []
             
-            # Change last update time to the newly changed date
-            lastUpdateDate = updatedate
-            
-            # Create lists to store market info
-            names = []
-            types = []
-            organizations = []
-            addresses = []
-            websites = []
-            open_days = []
-            open_times = []
-            close_times = []
-            open_months = []
-            vendor_numbers = []
-            offerings = []
-            checkedOfferings = []
-            lats = []
-            lons = []
-            
-            for row in cr:
-                # Only iterate and parse when the name field is not empty
-                if row[1] != '':
-                    names.append(row[1])
-                    types.append(row[0])
-                    organizations.append(row[2])
-                    addresses.append(row[7])
-                    websites.append(row[9])
-                    open_days.append(row[10])
-                    open_times.append(row[11])
-                    close_times.append(row[12])
-                    open_months.append(row[13])
-                    vendor_numbers.append(row[14])
-                    offerings.append(row[15])
-                    # Geocode address string into lat/lon and put into lats/lons list
-                    self.geocodeMarket(lats, lons, row[7])
+        for row in cr:
+            # Only iterate and parse when the name field is not empty
+            # Assumption: if market data is filled out, then name must be always present
+            if row[1] != None:
+                names.append(row[1])
+                types.append(row[0])
+                organizations.append(row[2])
+                addresses.append(row[7])
+                websites.append(row[9])
+                open_days.append(row[10])
+                open_times.append(row[11])
+                close_times.append(row[12])
+                open_months.append(row[13])
+                vendor_numbers.append(row[14])
+                offerings.append(row[15])
+                # Geocode address string into lat/lon and put into lats/lons list
+                self.geocodeMarket(lats, lons, row[7])
                 
-            # Check if there are empty fields and change empty fields to N/A
-            checkedOfferings = self.checkEmpty(offerings)
-            vendor_numbers = self.checkEmpty(vendor_numbers)
+        # Check if there are empty fields and change empty fields to N/A
+        offerings = self.checkEmpty(offerings)
+        vendor_numbers = self.checkEmpty(vendor_numbers)
+        types = self.checkEmpty(types)
+        organizations = self.checkEmpty(organizations)
+        addresses = self.checkEmpty(addresses)
+        websites = self.checkEmpty(websites)
+        open_days = self.checkEmpty(open_days)
+        open_times = self.checkEmpty(open_times)
+        close_times = self.checkEmpty(close_times)
+        open_months = self.checkEmpty(open_months)
+        print offerings
             
-            # Retrieved parsed information    
-            open_time_ints = self.convertTimeTo24Hr(open_times)
-            close_time_ints = self.convertTimeTo24Hr(close_times)
-            open_month_ints, close_month_ints = self.getOpenCloseMonths(open_months)
+        # Retrieved parsed information    
+        open_time_ints = self.convertTimeTo24Hr(open_times)
+        close_time_ints = self.convertTimeTo24Hr(close_times)
+        open_month_ints, close_month_ints = self.getOpenCloseMonths(open_months)
             
-            print vendor_numbers
-            
-            # This is used as a test to check if info are getting parsed correctly
-            return {'names':names,
-                    'types':types,
-                    'organizations': organizations,
-                    'addresses': addresses,
-                    'urls': websites,
-                    'open_days': open_days,
-                    'open_times': open_times,
-                    'close_times': close_times,
-                    'open_months': open_months,
-                    'vendors': vendor_numbers,
-                    'offerings': checkedOfferings,
-                    'open_time_ints': open_time_ints,
-                    'close_time_ints': close_time_ints,
-                    'open_month_ints': open_month_ints,
-                    'close_month_ints': close_month_ints,
-                    'lats': lats,
-                    'lons': lons
-                    }
+        # This is used as a test to check if info are getting parsed correctly
+        return {'names':names,
+                'types':types,
+                'organizations': organizations,
+                'addresses': addresses,
+                'urls': websites,
+                'open_days': open_days,
+                'open_times': open_times,
+                'close_times': close_times,
+                'open_months': open_months,
+                'vendors': vendor_numbers,
+                'offerings': offerings,
+                'open_time_ints': open_time_ints,
+                'close_time_ints': close_time_ints,
+                'open_month_ints': open_month_ints,
+                'close_month_ints': close_month_ints,
+                'lats': lats,
+                'lons': lons
+                }
       
     # Splice the OpenMonths into the month it opens and the month it closes     
     def getOpenCloseMonths(self, monthlist):
@@ -159,23 +154,24 @@ class MarketParser:
 
         # Create dictionary that maps Month to the corresponding integer
         monthDict = {
-                     'January':1, 'February':2, 'March':3,
-                     'April':4, 'May':5, 'June':6,
-                     'July':7, 'August':8, 'September':9,
-                     'October':10, 'November':11, 'December':12
-                     }
+                     'january':1, 'jan':1, 'february':2, 'feb':2, 'march':3, 'mar':3,
+                    'april':4, 'apr':4, 'may':5, 'june':6, 'jun':6,
+                    'july':7, 'jul':7, 'august':8, 'aug':8, 'september':9, 'sep':9,
+                    'october':10, 'oct':10, 'november':11, 'nov':11, 'december':12, 'dec':12
+                    }
     
         # Split the month range, which is given as: Month1-Month2, at hyphen
         for monthrange in monthlist:
-            months = re.split(r'-', ''.join(monthrange))
+            months = re.split(r'-|to', ''.join(monthrange))
+            refinedMonthRange = []
             # Use the monthDict and get corresponding integer value of the opening and closing months
-            if months[0].strip() in monthDict:
-                openMonths.append(monthDict.get(months[0].strip()))
-                closeMonths.append(monthDict.get(months[1].strip()))
-            # Handle Yaletown Farmer's market case (Mid-August to September)
-            else:
-                openMonths.append(monthDict.get(months[1].split()[0]))
-                closeMonths.append(monthDict.get(months[1].split()[2]))
+            for month in months:
+                if(month.strip().lower() in monthDict):
+                    intMonth = monthDict.get(month.strip().lower())
+                    refinedMonthRange.append(intMonth)
+            if refinedMonthRange:
+                openMonths.append(refinedMonthRange[0])
+                closeMonths.append(refinedMonthRange[1])
         return openMonths, closeMonths
 
     # Convert the open and close time of the markets
@@ -183,37 +179,34 @@ class MarketParser:
         market24Time = []
         # Iterate all the times given in the timelist
         for twelvetime in timelist:
-            # Convert list to string
-            stringTime = ''.join(twelvetime)
+            if twelvetime is not 'N/A':
+                # Convert list to string
+                stringTime = ''.join(twelvetime).lower()
         
-            # Take off am and pm and convert time to int
-            ampmtime = re.findall(r'\d+', twelvetime) 
-            ampmtime = int(''.join(ampmtime))
+                # Take off am and pm and convert time to int
+                ampmtime = re.findall(r'\d+', twelvetime) 
+                ampmtime = int(''.join(ampmtime))
             
-            # 12pm = 1200
-            if(ampmtime == 12 and re.search('pm', stringTime)):
-                ampmtime = 12
-            # 12am = 0000
-            elif(ampmtime == 12 and re.search('am', stringTime)):
-                ampmtime = 0
-            # all other pm = time + 12
-            elif re.search('pm', stringTime):
-                ampmtime = ampmtime + 12
-            # otherwise am time stays the same
-            # Add the converted time to the new list and return the list
-            market24Time.append(ampmtime)
+                # 12pm = 1200
+                if(ampmtime == 12 and re.search('pm', stringTime)):
+                    ampmtime = 12
+                # 12am = 0000
+                elif(ampmtime == 12 and re.search('am', stringTime)):
+                    ampmtime = 0
+                # all other pm = time + 12
+                elif re.search('pm', stringTime):
+                    ampmtime = ampmtime + 12
+                # otherwise am time stays the same
+                # Add the converted time to the new list and return the list
+                market24Time.append(ampmtime)
+            else:
+                market24Time.append(None)
         return market24Time 
     
-    # TESTING UPDATE
-    testingdate = " "
-    numUpdate = 0
     
-    # Testing
+    # Merge all the parsing and run
     def testRun(self):
-        cr = self.getCSV()
-        # --Can be taken out after--
-        updateRow = self.getUpdateRow(cr)
-        updateDate = self.getUpdateDate(updateRow)
-        # --------------------------
-        self.removeFieldNames(cr)
-        return self.parseMarketInfo(cr, self.testingdate, updateDate, self.numUpdate)
+        if (self.updateTime() is True):
+            cr = self.getCSV()
+            self.removeUpdateRowandFieldNames(cr)
+            return self.parseMarketInfo(cr)
